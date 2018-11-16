6f0d7ab1dd3c27b28755e83b1d0c033dc99b3211
JmeterChen <34080816+JmeterChen@users.noreply.github.com>
2018-11-16 17:34:09 +0800

    Wood_API_update
    wood接口用例更新

--- API_study/Wood/run_case.py
+++ API_study/Wood/run_case.py
@@ -6,15 +6,16 @@ import sys
 # import Open_file
 # import Rename_file
 # import Delete_file
-# import Logout
+import Logout
+
 
 if __name__ == '__main__':
-    # suite = unittest.TestSuite()
-    # suite.addTest(Logout.Logout('test_logout'))
+    suite = unittest.TestSuite()
+    suite.addTest(Logout.Logout('test_logout'))
     # suite.addTest(case02.Case02('test_case02'))
     # suite.addTest(case03.Case03('test_case03'))
     # suite.addTest(case04.Case04('test_case04'))
     # unittest.TextTestRunner().run(suite)
-    suite = unittest.defaultTestLoader.discover(start_dir=sys.argv[0]+'\\..\\', pattern='*.py')
+    # suite = unittest.defaultTestLoader.discover(start_dir=sys.argv[0]+'\\..\\', pattern='*.py')
     HTMLTestRunnerCN.HTMLTestRunner(stream=open(sys.argv[0]+'\\..\\report.html', 'wb'), title='Wood_apiAutoTest', description='WoodAPI_demo' ,tester='ChenBolin').run(suite)
     # unittest.TextTestRunner().run(suite)
